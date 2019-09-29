6de97901be7e4cf0ff3b5a0972f1d3c45d618924
long.chen <long.chen@symbio.com>
2019-09-29 15:09:04 +0800

    inin the project

---
+++ src/main/java/com/coin/platform/CoinPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.coin.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinPlatformApplication.class, args);
+	}
+
+}
